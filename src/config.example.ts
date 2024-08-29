f77f130b56347f676a43ed09185d19b3f7e3a264
Zahir Saiyed <zahir19saiyed@gmail.com>
2024-08-29 18:46:14 -0400

    bug fixes

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,13 +1,15 @@
+const clientId = process.env.AZURE_CLIENT_ID;
+
 export const msalConfig = {
     auth: {
-      clientId: "YOUR_CLIENT_ID",
-      authority: "https://login.microsoftonline.com/common",
-      redirectUri: "http://localhost:3000",
+      clientId: clientId!,
+      authority: 'https://login.microsoftonline.com/common',
+      redirectUri: process.env.REDIRECT_URI || 'http://localhost:3000',
     },
     cache: {
       cacheLocation: "sessionStorage",
       storeAuthStateInCookie: false,
     },
-};
-
-export const graphScopes = ['User.Read', 'Contacts.Read', 'Mail.Read'];
+  };
+  
+  export const graphScopes = ['User.Read', 'Contacts.Read', 'Mail.Read'];
